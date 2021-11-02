c57b8ca263754c2212d26fd36e7219a8d40d2281
Mo Abjal <mohdafzal330@gmail.com>
2021-11-02 23:12:06 +0530

    managed input output

--- Main.java
+++ Main.java
@@ -1,19 +1,34 @@
+
+import java.io.File;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
-        Scanner scn = new Scanner(System.in);
-        int n = scn.nextInt();
-        String str = "";
-        for(int i=0;i<n/2;i++){
-            str +="a";
-        }
-        str+="b";
-        for(int i=n/2;i<n;i++){
-            str +="a";
-        }
-        System.out.println(str);
+    static boolean useFiles = true;
+    static Scanner scn;
+    public static void main(String[] args) throws Exception {
+        setInputOutput();
 
+       int N = scn.nextInt();
+        for(int i=1;i<=N;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*\t");
+            }
+            System.out.println();
+        }
         scn.close(); 
+        
+    }
+
+
+
+    public static void setInputOutput() throws Exception{
+        if(useFiles){
+            scn = new Scanner(new File("input.txt"));
+            System.setOut(new PrintStream(new File("output.txt")));
+        } else {
+            scn = new Scanner(System.in);
+        }
+
     }
 }
